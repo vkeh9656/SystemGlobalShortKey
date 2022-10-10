34d1b723f86ad30c186635e0e0f51bccaf32d86b
DESKTOP-9FFSOO7\tjdwn <tjdwns9656@naver.com>
2022-10-11 02:44:15 +0900

    초기 커밋.

---
+++ SystemGlobalShortKeyDlg.h
@@ -0,0 +1,37 @@
+﻿
+// SystemGlobalShortKeyDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CSystemGlobalShortKeyDlg 대화 상자
+class CSystemGlobalShortKeyDlg : public CDialogEx
+{
+private:
+	char m_show_flag = 1;
+// 생성입니다.
+public:
+	CSystemGlobalShortKeyDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SYSTEMGLOBALSHORTKEY_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnHotKey(UINT nHotKeyId, UINT nKey1, UINT nKey2);
+};
